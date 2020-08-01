6741340a23c5c1e627ac62124664da8d324fa2bb
Den Pol <selection4test@gmail.com>
2020-08-02 01:56:57 +0300

    TPF-0 json viewer added

--- src/pages/training/use-reducer/UseReducer.tsx
+++ src/pages/training/use-reducer/UseReducer.tsx
@@ -7,8 +7,20 @@ import { Container } from '@material-ui/core'
 // import { MultilingualContext } from '@/common/context/mutilingual'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
+import ReactJson from 'react-json-view'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import PlusOneIcon from '@material-ui/icons/PlusOne'
+import ExposureNeg1Icon from '@material-ui/icons/ExposureNeg1'
 
 const useStyles = makeStyles((theme) => ({
+  title: {
+    marginBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
   button: {
     margin: theme.spacing(1),
   },
@@ -45,34 +57,42 @@ export const Links = () => {
 
   return (
     <Container>
-      <h1>
-        <code>useReducer</code>
-      </h1>
-      <Button
-        // type="submit"
-        // fullWidth
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        // disabled={isDisabled}
-        onClick={handleInc}
-        title="INC"
-      >
-        INC
-      </Button>
-      <Button
-        // type="submit"
-        // fullWidth
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        // disabled={isDisabled}
-        onClick={handleDec}
-        title="DEC"
-      >
-        DEC
-      </Button>
-      <p>{count.value}</p>
+      <Typography component="h1" variant="h5" className={classes.title}>
+        useReducer
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={3} lg={2} xl={2}>
+          <Button
+            // type="submit"
+            // fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            // disabled={isDisabled}
+            onClick={handleInc}
+            title="INC"
+          >
+            <PlusOneIcon />
+          </Button>
+          <Button
+            // type="submit"
+            // fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            // disabled={isDisabled}
+            onClick={handleDec}
+            title="DEC"
+          >
+            <ExposureNeg1Icon />
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={12} md={9} lg={10} xl={10}>
+          <Paper className={classes.paper}>
+            <ReactJson src={count} />
+          </Paper>
+        </Grid>
+      </Grid>
       <ProTip />
     </Container>
   )
